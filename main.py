34db56ba3c4af31b82218bb41095565bfef61b2d
Owner <reikhun2@gmail.com>
2026-02-19 19:48:55 +0630

    code 1 push

---
+++ main.py
@@ -0,0 +1,9 @@
+class cmp242Coders:
+    def __init__(self):
+        self .__grade = None
+
+    def set_grade(self, score):
+        self.__grade = score
+
+    def __getattr__(name):
+        return self .__grade
