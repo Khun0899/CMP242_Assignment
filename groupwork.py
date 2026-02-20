b109917bfe789598fd49ffdc11e4951fb5314c17
Owner <reikhun2@gmail.com>
2026-02-20 13:40:42 +0630

    groupwork push

---
+++ groupwork.py
@@ -0,0 +1,11 @@
+class GroupWork:
+    def presentation(self):
+        return "This presented a good work."
+
+class GoodPresentation(GroupWork):
+    def presentation(self):
+        return "This presentation was awesome."
+
+class GoodPresenter(GroupWork):
+    def presentation(self):
+        return "Good Presente."
